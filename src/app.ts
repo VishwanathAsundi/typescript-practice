66fe872f5a1540ab72efa77fb1fd2fa0d2d546bf
VishwanathAsundi <itzme.vishwanath.asundi@gmail.com>
2021-02-04 16:15:38 +0530

    ineterface indexed props, type casting

--- src/app.ts
+++ src/app.ts
@@ -1,111 +1,37 @@
-// abstract class Department {
-//     // private readonly id: string;
-//     // private name: string;
-//     protected employees: string[] = [];
+//custom type 1
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+type Employee = {
+    name: string;
+    startDate: Date;
+}
 
-//     constructor(protected readonly id: string, public name: string) {
-//         // this.id = id;
-//         // this.name = n;
-//     }
+type combinedType = Admin & Employee;
 
-//     abstract describe(this: Department): void;
+type unknownType = Admin | Employee;
 
-//     addEmployee(employee: string) {
-//         // validation etc
-//         // this.id = 'd2';
-//         this.employees.push(employee);
-//     }
+function printResult(e1: unknownType) {
+    console.log(e1.name);
+    if ('startDate' in e1) {
+        console.log(e1.startDate);
+    }
+    if ('privileges' in e1) {
+        console.log(e1.privileges);
+    }
 
-//     printEmployeeInformation() {
-//         console.log(this.employees.length);
-//         console.log(this.employees);
-//     }
-// }
+}
+printResult({ name: 'Vishwa', startDate: new Date() })
 
-// class ITDepartment extends Department {
-//     admins: string[];
-//     constructor(id: string, admins: string[]) {
-//         super(id, 'IT');
-//         this.admins = admins;
-//     }
-//     describe() {
-//         console.log("It department id:", this.id);
-//     }
-// }
+let inputEl = document.getElementById('hello-btn')! as HTMLInputElement;
 
-// class AccountingDepartment extends Department {
-//     lastReport: string;
-//     private static instance: AccountingDepartment;
+inputEl.value = "hey";
 
-//     private constructor(id: string, private reports: string[]) {
-//         super(id, 'Accounting');
-//         this.lastReport = reports[reports.length - 1];
-//     }
-//     static getInstance() {
-//         if (this.instance) {
-//             return this.instance;
-//         }
-//         this.instance = new AccountingDepartment('64735', []);
-//         return this.instance;
-
-//     }
-
-//     //getter method 
-//     get getMostRecentReport() {
-//         if (this.lastReport) {
-//             return this.lastReport;
-//         }
-//         throw new Error('No report found');
-//     }
-
-//     //setter
-//     set getMostRecentReport(value: string) {
-//         if (!value) {
-//             throw new Error('Please pass in valid value');
-//         }
-//         this.addReport(value);
-//     }
-
-//     addReport(text: string) {
-//         this.reports.push(text);
-//         this.lastReport = text;
-//     }
-
-//     printReports() {
-//         console.log(this.reports);
-//     }
-//     describe() {
-//         console.log("Acoounting department id:", this.id);
-//     }
-// }
-
-// const it = new ITDepartment('d1', ['Max']);
-
-// it.addEmployee('Max');
-// it.addEmployee('Manu');
-
-// // it.employees[2] = 'Anna';
-
-// it.describe();
-// it.name = 'NEW NAME';
-// it.printEmployeeInformation();
-
-// console.log(it);
-// it.describe();
-// const accounting = AccountingDepartment.getInstance();
-
-// accounting.getMostRecentReport = "new report";
-// accounting.addReport('Something went wrong...');
-// console.log(accounting.getMostRecentReport);
-
-// accounting.printReports();
-// // accounting.addEmployee("Vishwa");
-// // accounting.addEmployee("Netra");
-// // accounting.printEmployeeInformation();
-// accounting.describe();
-
-// // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-
-// // accountingCopy.describe();
-
-
+interface errorHandler {
+    //for dynamic property name
+    [prop: string]: string;
+}
+const myHandler: errorHandler = {
+    "hhh": "hello"
+}
